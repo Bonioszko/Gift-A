1a50a67e82f507ec8dc9f35d42c9988e48b0a233
Bonioszko <bartek.pers@gmail.com>
2024-06-17 11:54:21 +0200

    d

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         "O, patrz, pół roku",
         "O, jak romantycznie w walentynki",
         "Bardzo fajny ogród botaniczny",
-        "Patrz, tutaj nad morzem",
+        "Patrz, tutaj nad morze",
         "O, tutaj przed wyprawą 12 km...",
         "Bardzo fajne lustro",
         "A tutaj jacy szczęśliwi ze skałami",
@@ -44,7 +44,7 @@ function App() {
                     {!clicked ? (
                         <div
                             onClick={() => setClicked(true)}
-                            className="w-screen lg:h-screen bg-red-50 flex justify-center p-8 items-center"
+                            className="w-screen h-screen bg-red-50 flex justify-center p-8 items-center"
                         >
                             <h1 className=" text-2xl lg:text-7xl font-bold transition duration-3000 ease-in bg-gradient-to-r from-indigo-300 to-fuchsia-600 bg-clip-text text-transparent text-center">
                                 {" "}
@@ -54,7 +54,7 @@ function App() {
                             <div></div>
                         </div>
                     ) : (
-                        <div className="w-screen h-screen flex flex-col justify-around items-center bg-purple-200">
+                        <div className="w-screen min-h-screen lg:h-screen flex flex-col justify-around items-center bg-purple-200">
                             <div className="flex  flex-col gap-10  animate-slideIn">
                                 {" "}
                                 <h1 className=" text-5xl  font-bold bg-gradient-to-r from-indigo-300 to-fuchsia-600 bg-clip-text text-transparent text-center">
